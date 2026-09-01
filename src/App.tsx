85f17c24747f5a701c1af0cd876ac2d7e4119066
Gabo Herrera <herreragabriel1408@gmail.com>
2026-09-01 16:08:44 -0600

    Agregar la parte de equipo de trabajo

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Footer } from "./components/Footer/Footer";
 import { Home } from "./pages/Home/Home";
 import { Services } from "./pages/Services/Services";
 import { About } from "./pages/About/About";
+import { Team } from "./pages/Team/Team";
 import { Projects } from "./pages/Projects/Projects";
 import { Contact } from "./pages/Contact/Contact";
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/servicios" element={<Services />} />
         <Route path="/nosotros" element={<About />} />
+        <Route path="/equipo" element={<Team />} />
         <Route path="/proyectos" element={<Projects />} />
         <Route path="/contacto" element={<Contact />} />
       </Routes>
